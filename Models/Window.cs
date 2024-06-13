745cc3d428e79ba88b5529c5d434b8fe1abde33d
awwwsl <awwwsl.wdnmd@gmail.com>
2024-06-13 10:10:16 +0800

    Fix code style

--- Models/Window.cs
+++ Models/Window.cs
@@ -86,25 +86,20 @@ public class Window : GameWindow
                 Specular = (0f, 0f, 0.4f),
             }
         };
-        _chunk.Blocks[5,2,9] = new BlockPointLight("blockLamp")
+        _chunk.Blocks[5, 2, 9] = new BlockPointLight("blockLamp")
         {
-            Location = (5,2,9),
+            Location = (5, 2, 9),
             PointLight = new()
             {
-                Position = (5,2,9),
+                Position = (5, 2, 9),
                 Constant = 1f,
                 Linear = 0.09f,
                 Quadratic = 0.032f,
-                Ambient = (1.0f,1.0f,1.0f),
-                Diffuse = (1f,1f,1f),
-                Specular = (0.5f,0f,0f),
-
-
-
-
+                Ambient = (1.0f, 1.0f, 1.0f),
+                Diffuse = (1f, 1f, 1f),
+                Specular = (0.5f, 0f, 0f),
             }
         };
-
 
         _chunk.Load();
 
